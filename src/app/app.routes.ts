5bbde829168455c168b312ca8bfc366fd9d2d79c
Danilo Silva <danilo.silva@msn.com>
2025-05-17 15:26:06 -0300

    refactor: remover app.component.html e app.component.css não utilizados; atualizar app.component.ts para usar modelos e estilos em linha; configurar app.routes.ts com roteamento inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EntradaComponent } from './features/entrada/entrada.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: EntradaComponent },
+  { path: '**', redirectTo: '' },
+];
