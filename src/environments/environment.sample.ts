0bbed1befd9a9e5362b9570090698dc89be1341b
Danilo Silva <danilo.silva@msn.com>
2025-05-17 15:50:31 -0300

    feat: reformulação do entrada.component.html com um layout limpo, guias de navegação aprimoradas e seções de entrada do usuário aprimoradas para criar e ingressar em salas

---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'SUA_API_KEY_AQUI',
+    authDomain: 'seu-projeto.firebaseapp.com',
+    projectId: 'seu-projeto',
+    storageBucket: 'seu-projeto.appspot.com',
+    messagingSenderId: '123456789012',
+    appId: '1:123456789012:web:abcdef1234567890',
+  },
+};
