6cdf9dd3ab211f7f2505788515e699ec25ae91c9
Danilo Silva <danilo.silva@msn.com>
2025-05-17 15:04:45 -0300

    feat: adicionar a configuração CSS do Tailwind e integrá-la aos estilos; atualizar o layout do componente do aplicativo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "poker-blue": "#3f51b5",
+        "poker-light": "#e8eaf6",
+      },
+    },
+  },
+  plugins: [],
+};
