cfcba2b3866483facdc1cb182bb735be2cc3c86b
Danilo Silva <danilo.silva@msn.com>
2025-05-22 14:39:54 -0300

    docs: adicionar comentário explicativo no token de injeção de dependência do repositório de salas, melhorando a clareza do código

--- src/app/core/repositories/sala-repository.token.ts
+++ src/app/core/repositories/sala-repository.token.ts
@@ -1,4 +1,7 @@
 import { InjectionToken } from '@angular/core';
 import { ISalaRepository } from '../interfaces/sala-repository.interface';
 
+/**
+ * Token para injeção de dependência do repositório de salas
+ */
 export const SALA_REPOSITORY = new InjectionToken<ISalaRepository>('SalaRepository');
--- src/app/core/repositories/usuario-repository.token.ts
+++ src/app/core/repositories/usuario-repository.token.ts
@@ -1,4 +1,7 @@
 import { InjectionToken } from '@angular/core';
 import { IUsuarioRepository } from '../interfaces/usuario-repository.interface';
 
+/**
+ * Token para injeção de dependência do repositório de usuários
+ */
 export const USUARIO_REPOSITORY = new InjectionToken<IUsuarioRepository>('UsuarioRepository');
